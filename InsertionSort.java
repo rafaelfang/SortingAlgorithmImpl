6ab152fc98a3d3fedff66668eff6453ad9f83fd4
rafaelfang <cf797@mail.missouri.edu>
2014-09-14 23:08:42 -0500

    insertion srot

---
+++ InsertionSort.java
@@ -0,0 +1,29 @@
+public class InsertionSort {
+	public static void main(String[] args) {
+		int[] arr = { 3, 5, 2, 4, 7 };
+		printArr(arr);
+		insertionSort(arr);
+		printArr(arr);
+	}
+
+	public static void insertionSort(int[] arr) {
+
+		for (int i = 1; i < arr.length; i++) {
+			int current = arr[i];
+			int j = i;
+			while (j > 0 && arr[j - 1] > current) {
+				arr[j] = arr[j - 1];
+				j--;
+			}
+			arr[j] = current;
+		}
+
+	}
+
+	public static void printArr(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]);
+		}
+		System.out.println();
+	}
+}
